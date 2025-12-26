2686debcfd47fb9d2802385fe29556c30ba6d694
yaxyobekuz <pubgn9642@gmail.com>
2025-12-26 09:42:04 +0500

    feat: initialize React project with Vite, Tailwind CSS, and ESLint setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+// Styles
+import "./styles/index.css";
+
+// Components
+import App from "./App.jsx";
+
+// React
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(<App />);
