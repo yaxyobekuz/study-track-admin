43df96c0930d56faac15df8b9ce7eb5e405aa30b
yaxyobekuz <pubgn9642@gmail.com>
2025-12-26 10:58:15 +0500

    feat: integrate Toaster component with Sonner for enhanced notifications in RootLayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,8 +1,16 @@
 // Router
 import { Outlet } from "react-router-dom";
 
+// Components
+import { Toaster } from "@/components/ui/sonner";
+
 const RootLayout = () => {
-  return <Outlet />;
+  return (
+    <div className="">
+      <Outlet />
+      <Toaster />
+    </div>
+  );
 };
 
 export default RootLayout;
