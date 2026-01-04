7ea9f884745925c92222034b6ce252505f2f3165
yaxyobekuz <pubgn9642@gmail.com>
2026-01-04 19:57:45 +0500

    feat: implement Input component with support for text, textarea, and masked input types

---
+++ src/components/form/input.jsx
@@ -0,0 +1,117 @@
+import { cn } from "@/utils/tailwind.utils";
+import { InputMask } from "@react-input/mask";
+
+const Input = ({
+  value,
+  onChange,
+  name = "",
+  size = "lg",
+  label = "",
+  type = "text",
+  border = true,
+  className = "",
+  placeholder = "",
+  required = false,
+  disabled = false,
+  variant = "white",
+  ...props
+}) => {
+  const variantClasses = {
+    white: "bg-white",
+    gray: "bg-gray-50",
+    "gray-md": "bg-gray-100",
+  };
+
+  const sizeClasses = {
+    sm: "h-9 px-2 rounded-sm",
+    md: "h-10 px-3 rounded-md",
+    lg: "h-11 px-3.5 rounded-lg",
+    xl: "h-12 px-3.5 rounded-xl",
+  };
+
+  const defaultClasses = cn(
+    border ? "border border-gray-300" : "-outline-offset-1",
+    "w-full focus:outline-indigo-500"
+  );
+
+  const handleChange = (e) => {
+    onChange?.(type === "file" ? e.target.files : e.target.value);
+  };
+
+  const RenderInput = (() => {
+    if (type === "textarea") {
+      return (
+        <textarea
+          id={name}
+          {...props}
+          name={name}
+          value={value}
+          required={required}
+          disabled={disabled}
+          onChange={handleChange}
+          placeholder={placeholder}
+          className={cn(variantClasses[variant], defaultClasses)}
+        />
+      );
+    }
+
+    if (type === "tel") {
+      return (
+        <InputMask
+          id={name}
+          type="tel"
+          {...props}
+          name={name}
+          value={value}
+          required={required}
+          disabled={disabled}
+          onChange={handleChange}
+          placeholder={placeholder}
+          replacement={{ _: /\d/ }}
+          mask="+___ (__) ___-__-__"
+          className={cn(
+            variantClasses[variant],
+            defaultClasses,
+            sizeClasses[size]
+          )}
+        />
+      );
+    }
+
+    return (
+      <input
+        id={name}
+        {...props}
+        type={type}
+        name={name}
+        value={value}
+        required={required}
+        disabled={disabled}
+        onChange={handleChange}
+        placeholder={placeholder}
+        className={cn(
+          variantClasses[variant],
+          defaultClasses,
+          sizeClasses[size]
+        )}
+      />
+    );
+  })();
+
+  return (
+    <div className={cn("text-left space-y-1.5", className)}>
+      {label && (
+        <label
+          htmlFor={name}
+          className="ml-1 text-sm font-medium text-gray-700"
+        >
+          {label} {required && <span className="text-indigo-500">*</span>}
+        </label>
+      )}
+
+      {RenderInput}
+    </div>
+  );
+};
+
+export default Input;
