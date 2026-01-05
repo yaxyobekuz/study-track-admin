35bc98988781be19d1b5602de705be38331f131c
yaxyobekuz <pubgn9642@gmail.com>
2026-01-05 14:43:16 +0500

    feat: add responsive breakpoints to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,6 +55,15 @@ export default {
         },
       },
     },
+
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
