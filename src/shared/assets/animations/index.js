feaea1c6999ad066ed5122b5247f54d77cf34353
yaxyobekuz <pubgn9642@gmail.com>
2026-03-01 17:13:17 +0500

    feat: add emoji and duck animations to the animations module

---
+++ src/shared/assets/animations/index.js
@@ -0,0 +1,27 @@
+// Emoji Animations
+import timeEmojiAnimation from "./emoji/time.json";
+import magicEmojiAnimation from "./emoji/magic.json";
+import folderEmojiAnimation from "./emoji/folder.json";
+import greetingEmojiAnimation from "./emoji/greeting.json";
+import statsBarEmojiAnimation from "./emoji/stats-bar.json";
+
+// Duck Animations
+import pedroDuckAnimation from "./duck/pedro.json";
+import coolmanDuckAnimation from "./duck/coolman.json";
+import greetingDuckAnimation from "./duck/greeting.json";
+import blowingKissDuckAnimation from "./duck/blowing-kiss.json";
+
+export {
+  // Emoji Animations
+  timeEmojiAnimation,
+  magicEmojiAnimation,
+  folderEmojiAnimation,
+  greetingEmojiAnimation,
+  statsBarEmojiAnimation,
+
+  // Duck Animations
+  pedroDuckAnimation,
+  coolmanDuckAnimation,
+  greetingDuckAnimation,
+  blowingKissDuckAnimation,
+};
