606fcd14a605149c31cc7a21a941a5254a96ef67
yaxyobekuz <pubgn9642@gmail.com>
2026-03-14 15:00:56 +0500

    feat: add platforms data for admin, student, and teacher roles

---
+++ src/features/auth/data/platforms.data.js
@@ -0,0 +1,18 @@
+const platforms = [
+  {
+    name: "Admin 🧑‍💻",
+    isCurrent: true,
+  },
+  {
+    name: "O'quvchi 🧑‍🎓",
+    isCurrent: false,
+    href: "https://student.studytrack.uz",
+  },
+  {
+    name: "O'qituvchi 👨‍🏫",
+    isCurrent: false,
+    href: "https://teacher.studytrack.uz",
+  },
+];
+
+export default platforms;
