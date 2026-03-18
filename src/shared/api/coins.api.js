4d641ddb2b046bbf6bedba9b2577348301176349
yaxyobekuz <pubgn9642@gmail.com>
2026-03-18 14:12:33 +0500

    feat: remove all API files from shared/api as they are no longer used

--- src/shared/api/coins.api.js
+++
@@ -1,9 +0,0 @@
-import http from "@/shared/api/http";
-
-export const coinsAPI = {
-  getSettings: () => http.get("/coins/settings"),
-  updateSettings: (data) => http.put("/coins/settings", data),
-  getStats: () => http.get("/coins/stats"),
-  getStudentTransactions: (studentId, params) =>
-    http.get(`/coins/transactions/${studentId}`, { params }),
-};
