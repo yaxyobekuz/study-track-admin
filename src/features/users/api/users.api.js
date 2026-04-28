ff32e1b929a865d1539940504b0783479d0e4ed0
yaxyobekuz <pubgn9642@gmail.com>
2026-04-28 18:10:04 +0500

    feat: add getById function to usersAPI for retrieving user details by ID

--- src/features/users/api/users.api.js
+++ src/features/users/api/users.api.js
@@ -4,6 +4,7 @@ export const usersAPI = {
   getAll: (params) => http.get("/users", { params }),
   getAllShort: () => http.get("/users/all-short"),
   getStats: () => http.get("/users/stats"),
+  getById: (id) => http.get(`/users/${id}`),
   create: (data) => http.post("/users", data),
   update: (id, data) => http.put(`/users/${id}`, data),
   delete: (id) => http.delete(`/users/${id}`),
